622d86d95fc5b3c173e848b80648c21c3af9ff4a
Tiago Martins <tiago86martins@gmail.com>
2025-11-18 14:11:37 +0000

    added output writer to file

---
+++ singletons/OutputFileWriter.java
@@ -0,0 +1,25 @@
+package singletons;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class OutputFileWriter {
+	private static OutputFileWriter instance;
+
+	private OutputFileWriter() {}
+
+	public static OutputFileWriter.getInstance() {
+		if (instance == NULL) {
+			instance = new OutputFileWriter();
+		}
+		return instance;
+	}
+
+	public void writeOutput(String output) {
+		try (FileWriter writer = new FileWriter("simulation.txt", true)) {
+			writer.write(output + "\n");
+		} catch (IOException) {
+			System.out.println("There was an error while trying to write to 'simulation.txt'.")
+		}
+	}
+}
