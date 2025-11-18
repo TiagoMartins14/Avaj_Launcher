ad8df87d55059d8e0e038b519aa2c184b036dabf
Tiago Martins <tiago86martins@gmail.com>
2025-11-18 15:44:33 +0000

    format

--- buildings/Flyable.java
+++ buildings/Flyable.java
@@ -4,6 +4,7 @@ public abstract class Flyable {
     protected WeatherTower weatherTower;
 
     public abstract void updateConditions();
+
     public registerTower(WeatherTower p_tower) {
         this.weatherTower = p_tower;
     }
