02c9eeaaa269f59f36efc22716c35f5340359bc4
Tiago Martins <tiago86martins@gmail.com>
2025-11-18 15:43:56 +0000

    added functions logic

--- buildings/Tower.java
+++ buildings/Tower.java
@@ -1,11 +1,41 @@
 package buildings;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import singletons.OutputFileWriter;
+import buildings.Flyable;
+
 public class Tower {
-    private List<Flyable> observers;
+    private List<Flyable> observers = new ArrayList<>();
 
-    public void register(Flyable p_flyable) {}
+    public void register(Flyable p_flyable) {
+        observers.add(p_flyable);
+        
+        OutputFileWriter fileWriter = OutputFileWriter.getInstance();
 
-    public void unregister(Flyable p_flyable) {}
+        fileWriter.writeOutput("Tower says: " + p_flyable.getTag() + " registered to weather tower.");
+    }
 
-    protected void conditionChanged() {}
+    public void unregister(Flyable p_flyable) {
+        OutputFileWriter fileWriter = OutputFileWriter.getInstance();
+        
+        fileWriter.writeOutput(p_flyable.getTag() + ": landing.");
+        fileWriter.writeOutput("Tower says: " + p_flyable.getTag() + " unregistered from weather tower.");
+    }
+
+    protected void conditionChanged() {
+        Iterator<Flyable> it = observers.iterator();
+
+        while (it.hasNext()) {
+            Flyable flyable = it.next();
+
+            flyable.updateConditions();
+            
+            if (flyable.hasLanded()) {
+                unregister(flyable);
+                it.remove();
+            }
+        }
+    }
 }
