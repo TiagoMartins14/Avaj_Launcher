10318cbea0791dd741c7bc84adfc79deb9a4ac0e
Tiago Martins <tiago86martins@gmail.com>
2025-11-18 12:37:13 +0000

    added constants

---
+++ constants.java
@@ -0,0 +1,13 @@
+package constants;
+
+// Weather
+String RAIN = "RAIN";
+String FOG = "FOG";
+String SUN = "SUN";
+String SNOW = "SNOW";
+
+// Aircrafts
+String JETPLANE = "JetPlane";
+String HELICOPTER = "Helicopter";
+String BALLOON = "Balloon";
+
