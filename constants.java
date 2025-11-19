e82dd9406fe6e2fe06775160f79bc2ef3a82f3f5
Tiago Martins <tiago86martins@gmail.com>
2025-11-19 09:39:48 +0000

    upgraded to interface

--- constants.java
+++ constants.java
@@ -1,13 +1,15 @@
 package constants;
 
-// Weather
-String RAIN = "RAIN";
-String FOG = "FOG";
-String SUN = "SUN";
-String SNOW = "SNOW";
 
-// Aircrafts
-String JETPLANE = "JetPlane";
-String HELICOPTER = "Helicopter";
-String BALLOON = "Balloon";
-
+public interface Constants {
+	// Weather
+	String RAIN = "RAIN";
+	String FOG = "FOG";
+	String SUN = "SUN";
+	String SNOW = "SNOW";
+	
+	// Aircrafts
+	String JETPLANE = "JetPlane";
+	String HELICOPTER = "Helicopter";
+	String BALLOON = "Balloon";
+}
