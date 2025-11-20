6c5b6e9e362be585951cb20d3aad6ac15e7644c6
Tiago Martins <tiago86martins@gmail.com>
2025-11-20 16:30:47 +0000

    running simulator

--- simulator/Simulator.java
+++ simulator/Simulator.java
@@ -1,87 +1,73 @@
 package simulator;
 
-import static constants.Constants.*;
 import buildings.*;
 import flyables.*;
+import flyables.aircrafts.Coordinates;
 import singletons.*;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Simulator {
+	private long numberOfSimulations;
+	private WeatherTower weatherTower = new WeatherTower();
+	private AircraftFactory aircraftFactory = AircraftFactory.getInstance();
+
+	private void runSimulator(File scenario) {
+		Validator myValidator = new Validator(scenario);
+		
+		if (myValidator.isValidFile()) {
+
+			try {
+        		new FileWriter("simulation.txt", false).close();
+    		} catch (IOException e) {
+       			System.out.println("Could not reset simulation.txt");
+    		}
+
+			try (Scanner scanner = new Scanner(scenario)) {
+				// Get number of simulations
+				numberOfSimulations = Integer.parseInt(scanner.nextLine().trim());
+
+				// Get number of aicrafts
+				while (scanner.hasNextLine()) {
+					String[] aircraftParams =  scanner.nextLine().trim().split("\\s+");
+
+					String type = aircraftParams[0];
+					String name = aircraftParams [1];
+					int lon = Integer.parseInt(aircraftParams[2]);
+					int lat = Integer.parseInt(aircraftParams[3]);
+					int height = Integer.parseInt(aircraftParams[4]);
+
+					Coordinates coordinates = new Coordinates(lon, lat, height);
+					Flyable newAircraft = aircraftFactory.newAircraft(type, name, coordinates);
+
+					// aircrafts.add(newAircraft);
+					newAircraft.registerTower(weatherTower);
+				}
+				
+				// Run sim <numberOfSimulations> times
+				for (long i = 0; i < numberOfSimulations; i++) {
+					weatherTower.changeWeather();
+				}
+			
+			} catch (FileNotFoundException e) {
+				System.out.println("File not found: " + scenario.getName());
+			}
+		} 
+	}
+
 	public static void main(String[] args) {
 		if (args.length != 1) {
 			return;
 		}
+		
 
-		String fileName = args[0];
-		File scenario = new File(fileName);
+		final File scenario = new File(args[0]);
 
-		try (Scanner myReader = new Scanner(scenario)) {
-			if (isFirstLineGood(myReader) && areRemainingLinesGood(myReader)) {
-				//
-			} else {
-				System.out.println("File incorrectly formatted.");
-			}
-		} catch (FileNotFoundException e) {
-			System.out.println("File not found: " + fileName);
-		}
-	}
+		final Simulator simulator = new Simulator();
 
-	private static boolean isFirstLineGood(Scanner myReader) {
-		if (!myReader.hasNextLine()) {
-			System.out.println("File is empty.");
-			return false;
-		}
-
-		String data = myReader.nextLine().trim();
-
-		if (!isPositiveLong(data)) {
-			System.out.println("Incorrect file parameter(s).");
-			return false;
-		}
-
-		return true;
-	}
-
-	private static boolean areRemainingLinesGood(Scanner myReader) {
-		while (myReader.hasNextLine()) {
-			String line = myReader.nextLine().trim();
-			String[] params = line.split("\\s+");
-
-			if (params.length != 5) {
-				return false;
-			}
-
-			if (!params[0].equals(BALLOON) &&
-				!params[0].equals(HELICOPTER) &&
-				!params[0].equals(JETPLANE)) {
-				return false;
-			}
-
-			int lon, lat, height;
-			try {
-				lon = Integer.parseInt(params[2]);
-				lat = Integer.parseInt(params[3]);
-				height = Integer.parseInt(params[4]);
-			} catch (NumberFormatException e) {
-				return false;
-			}
-
-			if (lon < 0 || lat < 0 || height < 0 || height > 100) {
-				return false;
-			}
-		}
-
-		return true;
-	}
-
-	private static boolean isPositiveLong(String str) {
-		try {
-			long value = Long.parseLong(str);
-			return value > 0;
-		} catch (NumberFormatException e) {
-			return false;
-		}
+		simulator.runSimulator(scenario);
 	}
 }
